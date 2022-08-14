3654d1a309bb169f4f16e5fd30676f57bca87965
Charlie Carter <charliec1665@gmail.com>
2022-08-14 15:56:12 -0400

    get user repos function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiURL).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 }
 
 getUserRepos();
