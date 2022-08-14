566aa6e039de121444c7c44e43f2b5201b1e2903
Charlie Carter <charliec1665@gmail.com>
2022-08-14 16:36:08 -0400

    repo search plus issue count

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -51,6 +51,21 @@ var displayRepos = function(repos, searchTerm) {
         // append to container
         repoEl.appendChild(titleEl);
 
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if(repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =
+            "<i class ='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append to container
+        repoEl.appendChild(statusEl);
+
         // append container to the DOM
         repoContainerEl.appendChild(repoEl);
     }
